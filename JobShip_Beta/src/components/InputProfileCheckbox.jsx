5c890f93c375563ac500f4db53e4190452eec597
Mirai-123123123 <gunshinmi@gmail.com>
2023-03-21 22:32:48 +0900

    　

---
+++ JobShip_Beta/src/components/InputProfileCheckbox.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const InputProfileCheckbox = () => {
+  return (
+    <div>InputProfileCheckbox</div>
+  )
+}
+
+export default InputProfileCheckbox
---
+++ JobShip_Beta/src/components/InputProfileImage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const InputProfileImage = () => {
+  return (
+    <div>InputProfileImage</div>
+  )
+}
+
+export default InputProfileImage
