5d4158be0d521085bd2e28e5412163da420ce3c9
samachi-hcm <samachi.hcm@gmail.com>
2023-07-28 17:57:12 +0900

    🐛フッターの表示バグ修正

--- JobShip_Beta/src/components/Footer.jsx
+++ JobShip_Beta/src/components/Footer.jsx
@@ -7,17 +7,17 @@ import { Link } from 'react-router-dom';
 import './css/Footer.css'
 
 const Footer = () => {
-  
+
   const JobRecord_IconRE = {
-    imgAddess:"/JobRecord_iconRE.png"
+    imgAddess: "/JobRecord_iconRE.png"
   }
 
   const JobRecord_LogoRE = {
-    imgAddess:"/JobRecord_LogoRE.png"
+    imgAddess: "/JobRecord_LogoRE.png"
   }
 
   const Shioh_LogoRE = {
-    imgAddess:"/Shioh_LogoRE.png"
+    imgAddess: "/Shioh_LogoRE.png"
   }
 
   const navigate = useNavigate()
@@ -35,30 +35,34 @@ const Footer = () => {
   }
 
   return (
-    <Container fluid style={{backgroundColor:"#7233B4",height:"110px"}}>
+    <Container fluid style={{ backgroundColor: "#7233B4", height: "110px" }}>
       <Row>
-      <Link to="/">
-        <Col style={{paddingTop:"20px",paddingLeft:"10px",height:"60px"}} xs="auto">
-          <img src={JobRecord_IconRE.imgAddess} style={{height:"70%"}}/>
-          <img src={JobRecord_LogoRE.imgAddess} style={{height:"80%",marginLeft:"5px"}}/>
+
+        <Col style={{ paddingTop: "20px", paddingLeft: "10px", height: "60px" }} xs="auto">
+          <Link to="/" style={{ margin: "0" }}>
+            <img src={JobRecord_IconRE.imgAddess} style={{ height: "70%" }} />
+            <img src={JobRecord_LogoRE.imgAddess} style={{ height: "80%", marginLeft: "5px" }} />
+          </Link>
         </Col>
-      </Link>
-        <Col md={{offset:"5",span:"auto"}} lg={{offset:"7",span:"auto"}} style={{paddingTop:"25px",fontSize:"small",color:"#ffffff", textAlign:'right'}}>
-        <a onClick={()=>toPrivacyPolicy()}>プライバシーポリシー</a>
+
+        <Col md={{ offset: "5", span: "auto" }} lg={{ offset: "7", span: "auto" }} style={{ paddingTop: "25px", fontSize: "small", color: "#ffffff", textAlign: 'right' }}>
+          <a onClick={() => toPrivacyPolicy()}>プライバシーポリシー</a>
         </Col>
-        <Col  md="auto" lg="auto" style={{paddingTop:"25px",fontSize:"small",color:"#ffffff", textAlign:'right'}}>
-        <a onClick={()=>toTermsOfService()}>利用規約</a>
+        <Col md="auto" lg="auto" style={{ paddingTop: "25px", fontSize: "small", color: "#ffffff", textAlign: 'right' }}>
+          <a onClick={() => toTermsOfService()}>利用規約</a>
         </Col>
-        <Col md="auto" lg="auto" style={{paddingTop:"25px",fontSize:"small",color:"#ffffff", textAlign:'right'}}>
-        <a onClick={()=>toCompanyWebsite()}>運営会社</a>
+        <Col md="auto" lg="auto" style={{ paddingTop: "25px", fontSize: "small", color: "#ffffff", textAlign: 'right' }}>
+          <a onClick={() => toCompanyWebsite()}>運営会社</a>
         </Col>
       </Row>
       <Row>
-       <Link to="/">
-        <Col style={{height:"50px",textAlign:"right",paddingRight:"30px",paddingTop:"10px"}} lg={{offset:"10",span:"2"}}>
-        <img src={Shioh_LogoRE.imgAddess} style={{height:"60%",marginLeft:"5px"}}/>
+
+        <Col style={{ height: "50px", textAlign: "right", paddingRight: "30px", paddingTop: "10px" }} lg={{ offset: "10", span: "2" }}>
+          <Link to="/" style={{ margin: "0" }}>
+            <img src={Shioh_LogoRE.imgAddess} style={{ height: "60%", marginLeft: "5px" }} />
+          </Link>
         </Col>
-        </Link>
+
       </Row>
     </Container>
   )
